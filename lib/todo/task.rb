f8960e09a32b4460be0cdc4d4d574f4d0850ca7a
tkak <keepkeptkept@gmail.com>
2014-08-10 11:43:53 +0900

    use css and add adding task function

--- lib/todo/task.rb
+++ lib/todo/task.rb
@@ -7,6 +7,10 @@ module Todo
   class Task < ActiveRecord::Base
     scope :status_is, ->(status) { where(status: status) }
 
+    scope :status_is_not_yet, -> { status_is(NOT_YET) }
+    scope :status_is_done, -> { status_is(DONE) }
+    scope :status_is_pending, -> { status_is(PENDING) }
+
     NOT_YET = 0
     DONE = 1
     PENDING = 2
